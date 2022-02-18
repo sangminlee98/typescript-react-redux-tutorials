1873f30c64a85f206d7b9a6ccc476a90558680ee
sangminlee98 <tkdals0975@naver.com>
2022-02-18 14:17:56 +0900

    modify action types

--- learn-redux-middleware/src/modules/counter.ts
+++ learn-redux-middleware/src/modules/counter.ts
@@ -1,29 +1,28 @@
 import { Dispatch } from 'react';
 
-type Type = 'counter/INCREASE' | 'counter/DECREASE';
-type Action = {type: Type};
+type CounterAction = ReturnType<typeof increase> | ReturnType<typeof decrease>;
 
-const INCREASE: Type = 'counter/INCREASE';
-const DECREASE: Type = 'counter/DECREASE';
+const INCREASE = 'counter/INCREASE' as const;
+const DECREASE = 'counter/DECREASE' as const;
 
 export const increase = () => ({type: INCREASE});
 export const decrease = () => ({type: DECREASE});
 
 
 // 1초 뒤에 increase 혹은 decrease 함수를 디스패치함
-export const increaseAsync = () => (dispatch: Dispatch<Action>) => {
+export const increaseAsync = () => (dispatch: Dispatch<CounterAction>) => {
   setTimeout(() => {
     dispatch(increase());
   }, 1000)
 }
-export const decreaseAsync = () => (dispatch: Dispatch<Action>) => {
+export const decreaseAsync = () => (dispatch: Dispatch<CounterAction>) => {
   setTimeout(() => {
     dispatch(decrease());
   }, 1000)
 }
 
 const initialState = 0;
-const counter = (state = initialState, action: Action) => {
+const counter = (state = initialState, action: CounterAction) => {
   switch(action.type) {
     case INCREASE:
       return state + 1;
